148c49f99c35317b2a0a4fd68a08ebe8e3b065dd
kawaiicakes <vomitmopper@gmail.com>
2025-11-21 04:05:12 -0700

    Fixed mixin previously causing failure to build.

--- src/main/java/net/junedev/junetech_geo/mixin/client/LevelLoadingScreenMixin.java
+++ src/main/java/net/junedev/junetech_geo/mixin/client/LevelLoadingScreenMixin.java
@@ -1,5 +1,6 @@
-/*package net.junedev.junetech_geo.mixin.client;
+package net.junedev.junetech_geo.mixin.client;
 
+import com.mojang.logging.LogUtils;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.junedev.junetech_geo.worldgen.chunk.JtGeOChunkStatus;
 import net.minecraft.client.gui.screens.LevelLoadingScreen;
@@ -11,11 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * add {@link JtGeOChunkStatus#STONE_REPLACER} to the level loading screen after {@link ChunkStatus#NOISE} with color {@code #127b81}
-
+ *
+*/
 @Mixin(LevelLoadingScreen.class)
 public class LevelLoadingScreenMixin {
-    @Inject(method = "lambda$static$1", at = @At(value = "HEAD", remap = false), remap = false)
+    @Inject(method = "lambda$static$0", at = @At(value = "HEAD"))
     private static void addStoneReplacer(Object2IntOpenHashMap<ChunkStatus> chunkStatus, CallbackInfo ci) {
+        LogUtils.getLogger().info("Hello from the Mixin!"); // remove this at some point, here for debug purposes
         chunkStatus.put(JtGeOChunkStatus.STONE_REPLACER, 0x127b81);
     }
-} */
+}
